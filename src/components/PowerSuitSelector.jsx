9788c429a429f8227cc527d79424db4e00c47c99
AbdulAzizKh4tri <abdulazizahk@gmail.com>
2026-03-14 01:02:13 +0530

    lowercase card suits bug fix

--- src/components/PowerSuitSelector.jsx
+++ src/components/PowerSuitSelector.jsx
@@ -10,10 +10,10 @@ const PowerSuitSelector = ({user, socket, theme, pgs}) => {
 		<div className="h-full w-full flex items-center justify-center p-5">
 			<div className="grid grid-cols-4 grid-rows-1 h-full w-full">
 			{
-				["spades", "hearts", "diamonds", "clubs"].map((suit, index) => {
+				["Spades", "Hearts", "Diamonds", "Clubs"].map((suit, index) => {
 					return (
 						<div key={suit} className="col-span-1 p-5 flex items-center justify-center">
-							<img src={`/assets/cards/${theme}/${suit}.png`} alt={`${suit}`} 
+							<img src={`/assets/cards/${theme}/${suit.toLowerCase()}.png`} alt={`${suit}`} 
 								className={`${disabled ? 'pointer-events-none opacity-70' : 'cursor-pointer hover:-translate-y-2 transition-transform'}
 									drop-shadow-[0_8px_12px_rgba(0,0,0,0.8)] drop-shadow-[0_2px_3px_rgba(0,0,0,0.9)] `}
 								onClick={() => {selectPowerSuit(suit)}}
